1be294a6b8b2f5c6d0f6e299e477adaadc8356fa
jheykocarreno <jheyko1000@gmail.com>
2025-03-20 17:54:51 -0300

    aula 20/03

---
+++ scripts/ex-condicionais.js
@@ -0,0 +1,185 @@
+//------------------------- NIVEL BASICO -------------------------
+//1- Verificar se um número é positivo, negativo ou zero
+// let numero = prompt("Inserir um numero");
+// numero = parseInt(numero);
+
+// if (numero > 0){
+//     console.log("Numero positivo");
+// } else if (numero < 0){
+//     console.log("Numero negativo");
+// } else {
+//     console.log("Numero é ZERO");
+// }
+
+
+//2- Verificar se um número é par ou ímpar
+// let numero2 = prompt("Inserir um numero");
+// numero2 = parseInt(numero2);
+
+// if (numero2 % 2 == 0){
+//     console.log("É numero PAR");
+// } else{
+//     console.log("É numero IMPAR")
+// }
+
+
+//3- Classificação de idade
+// let idade = prompt("Inserir idade");
+// idade = parseInt(idade);
+
+// if (idade < 12){
+//     console.log("Criança");
+// } else if (idade >=13 && idade <=17){
+//     console.log("Adolescente");
+// } else {
+//     console.log("Adulto");
+// }
+
+
+//4- Aprovação em uma prova
+// let nota = prompt("Digite una nota");
+// nota = parseInt(nota);
+
+// if (nota >= 60 && nota <= 100){
+//     console.log("Aprovado!");
+// } else if (nota >= 0 && nota <60){
+//     console.log("Reprovado");
+// } else {
+//     console.log("A nota deve ser de 0 ao 100");
+// }
+
+
+//------------------------- NIVEL INTERMEDIARIO -------------------------
+//1- Calculadora simples
+// let a = prompt("Inserir primeiro numero");
+// a = parseInt(a);
+// let b = prompt("Inserir segundo numero");
+// b = parseInt(b);
+// let oper = prompt("Inserir o operador: + - * /");
+// let resultado;
+
+// switch (oper){
+//     case '+':
+//         resultado = a + b;
+//         console.log(resultado);
+//         break;
+//     case '-':
+//         resultado = a - b;
+//         console.log(resultado);
+//         break;
+//     case '*':
+//         resultado = a * b;
+//         console.log(resultado);
+//         break;
+//     case '/':
+//         resultado = a / b;
+//         console.log(resultado);
+//         break;
+//     default:
+//         console.log("Somente aceita as operações + - * /");
+// }
+
+
+//2- Maior entre três números
+// let a = prompt("Primeiro numero");
+// let b = prompt("Segundo numero");
+// let c = prompt("Terceiro numero");
+
+// if (a>=b){
+//     if (a>=c){
+//         console.log("1- O numero  maior é " + a);
+//     } else {
+//         console.log("2- O numero  maior é " + c);
+//     }
+// }else {
+//     if (b>=c){
+//         console.log("3- O numero  maior é " + b);
+//     } else {
+//         console.log("4- O numero  maior é " + c);
+//     }
+// }
+
+
+//3- Desconto em compras
+// let valor = prompt("Inserir valor da compra");
+// valor = parseInt(valor);
+// let valorNovo;
+
+// if (valor >= 100){
+//     valorNovo = valor - (valor * 10/100);
+//     console.log("Teve un desconto de 10%. Valor a pagar: " + valorNovo);
+// } else {
+//     console.log("Valor a pagar: " + valor);
+// }
+
+
+//4- Sistema de login simples
+// const usuario = "admin";
+// const senha = "1234";
+
+// let usuEnt = prompt("Inserir nome de usuario");
+// let senEnt = prompt("Inserir senha");
+
+// if (usuario == usuEnt && senha == senEnt){
+//     console.log("Login bem-sucedido");
+// }else {
+//     console.log("Acesso negado");
+// }
+
+
+//------------------------- NIVEL AVANZADO -------------------------
+//1- Classificação de triângulos
+// let ladoA = prompt("Inserir o lado A");
+// let ladoB = prompt("Inserir o lado B");
+// let ladoC = prompt("Inserir o lado C");
+
+// ladoA = parseInt(ladoA);
+// ladoB = parseInt(ladoB);
+// ladoC = parseInt(ladoC);
+
+// if (ladoA == ladoB && ladoA == ladoC){
+//     console.log("É un Triandulo Equilatero");
+// } else {
+//     if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC){
+//         console.log("É um Triangulo Isosceles");
+//     } else{
+//         console.log("É um Triangulo Escaleno");
+//     }
+// }
+
+
+//2- Conversão de notas para conceitos
+//  let nota = parseInt(prompt("Digite una nota"));
+//  let resultOk = false;
+
+// if (nota <= 100 && nota >= 90){
+//     resultOk = true;
+//     console.log("Tem uma nota A");
+// }
+
+// if (nota < 90 && nota >= 80){
+//     resultOk = true;
+//     console.log("Tem uma nota B");
+// }
+
+// if (nota < 80 && nota >= 70){
+//     resultOk = true;
+//     console.log("Tem uma nota C");
+// }
+
+// if (nota < 70 && nota >= 60){
+//     resultOk = true;
+//     console.log("Tem uma nota D");
+// }
+
+// if (nota < 60 && nota >= 0){
+//     resultOk = true;
+//     console.log("Tem uma nota E");
+// }
+
+// if (resultOk == false){
+//     console.log("A nota deve ser entre 0 e 100");
+// }
+
+
+//3- Cálculo de IMC
--- scripts/script.js
+++ scripts/script.js
@@ -42,14 +42,113 @@
 // d = toString(d);
 // console.log(typeof d);
 
-let nome = "Jheyko";
-let idade = 41;
-const profissao = "Aluno";
+// let nome = "Jheyko";
+// let idade = 41;
+// const profissao = "Aluno";
 
-console.log("Ola, meu nome é " + nome + ", tenho " + idade + " anos de idade");
+// console.log("Ola, meu nome é " + nome + ", tenho " + idade + " anos de idade");
 
-let mensagem = "Ola. meu nome é " + nome;
-console.log(mensagem);
+// let mensagem = "Ola. meu nome é " + nome;
+// console.log(mensagem);
 
-mensagem = `Hola, mi nombre é ${nome}, y tengo ${idade} años. Soy ${profissao}`;
-console.log(mensagem);
+// mensagem = `Hola, mi nombre é ${nome}, y tengo ${idade} años. Soy ${profissao}`;
+// console.log(mensagem);
+
+//------------ CONDICIONALES -------------
+//IF simples
+// let idade = prompt("Digite sua idade: ");
+
+// if (idade >= 18){
+//     console.log("Voce é maior de idade");
+// }
+
+//IF e ELSE
+// let temperatura = 25;
+
+// if (temperatura > 25){
+//     console.log("Esta calor");
+// }else{
+//     console.log("Esta fresquinho");
+// }
+
+// IF - ELSE IF - ELSE
+// let nota = 11;
+
+// if (nota > 10){
+//     console.log ("A nota não pode ser maior de 10");
+// } else if (nota >= 6){
+//     console.log ("Aprovado");
+// } else {
+//     console.log ("Esta de recuperação");
+// }
+
+// OPERADOR TERNARIO / IF TERNARIO
+// let saldo = prompt("Digite saldo: ");
+// let podeComprar = saldo >= 100 ? "Compra aprovada" : "Saldo insuficiente";
+// console.log (podeComprar);
+
+//SWITCH
+// let dia = prompt("Digite o dia: ");
+// console.log(typeof dia);
+// switch (dia) {
+//     case '1':
+//         console.log("Domingo");
+//         break;
+//     case '2':
+//         console.log("Segunda-feira");
+//         break;
+//     case '3':
+//         console.log("Terça-feira");
+//         break;
+//     case '4':
+//         console.log("Quarta-feira");
+//         break;
+//     case '5':
+//         console.log("Quinta-feira");
+//         break;
+//     case '6':
+//         console.log("Sexta-feira");
+//         break;
+//     case '7':
+//         console.log("Sábado");
+//         break;
+//     default:
+//         console.log("Dia invalido, somente 1 ao 7");
+//         break;
+// }
+
+//CONDICIONAL ANINHADA
+//IF DENTRO DE IF
+// let idade = 19;
+// let temCarteira = true;
+
+// if (idade >= 18){
+//     if (temCarteira){
+//         console.log("Você pode dirigir");
+//     } else {
+//         console.log("Voce precisa tirar sua carterra de motorista");
+//     }
+// } else {
+//     console.log("Voce não pode dirigir");
+// }
+
+//CONDICAO ANINHADA
+//MULTIPLAS VERIFICAÇÕES
+
+let hora = prompt("Digita hora");
+hora = parseInt(hora);                      //convertemos String para Int
+console.log(hora);
+let diaDaSemana = prompt("Digita o dia");
+console.log(diaDaSemana);
+
+if (hora >= 6 && hora < 12){
+    console.log("Bom dia");
+} else {
+    if (hora >= 12 && hora < 18){
+        if (diaDaSemana == "segunda"){
+            console.log("Boa tarde! Ótima semana!");
+        } else {
+            console.log("Boa tarde");
+        }
+    }
+}
